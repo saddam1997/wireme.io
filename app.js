d2bef7a172a60583a2bea249538d0e46d16dcb4f
Rizvan <rzvnch@gmail.com>
2017-12-20 13:46:45 +0530

    init
    Signed-off-by: Rizvan <rzvnch@gmail.com>

--- app.js
+++ app.js
@@ -17,8 +17,7 @@
  * The same command-line arguments are supported, e.g.:
  * `node app.js --silent --port=80 --prod`
  */
-
-
+/*sdfdsfsafsdfsdaflsdjlfjlk*/
 // Ensure we're in the project directory, so cwd-relative paths work as expected
 // no matter where we actually lift from.
 // > Note: This is not required in order to lift, but it is a convenient default.
